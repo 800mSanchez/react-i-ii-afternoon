a437f7e90c20839b2525ff73ef9f801d7caf5847
Alejandro Sanchez <alejandrosanchez9494@gmail.com>
2020-07-23 16:22:12 -0500

    first test

---
+++ react-3-afternoon/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import data from "./data"
+
+function App() {
+  return (
+    <div className="App">
+      
+    </div>
+  );
+}
+
+export default App;
